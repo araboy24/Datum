f2d55c8aa3c34204e06b1477f447b0d596b9af68
Eid Zaben <eid.s.zaben-1@ou.edu>
2021-03-09 22:16:42 +0200

    Hooked up to Firebase

---
+++ lib/shared/loading.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:datum/shared/constants.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import 'constants.dart';
+
+class Loading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          'EZ-HAX',
+          style: TextStyle(
+              color: Colors.lightBlue, fontSize: 40, fontFamily: 'code_font'),
+        ),
+      ),
+      body: Container(
+       decoration: bgDec,
+        child: Center(
+          child: SpinKitCubeGrid(
+            color: Colors.blue,
+            size: 100.0,
+          ),
+        ),
+
+      ),
+
+    );
+  }
+}
